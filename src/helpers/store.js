e4195875bd677d45288465161b4b3207a87500e1
Abdelkrim Dib <abdelkrim-dev@outlook.com>
2018-05-26 15:42:29 +0100

    add reducers to the store

--- src/helpers/store.js
+++ src/helpers/store.js
@@ -1,9 +1,13 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 
+import { allUsers } from '../reducers/all-users.reducer';
+import { specificUser } from '../reducers/specific-user.reducer';
+
 export const store = createStore(
     combineReducers({
-
+        allUsers,
+        specificUser
     }),
     applyMiddleware(thunkMiddleware)
 );
